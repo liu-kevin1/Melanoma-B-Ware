75ead7b920c61759aca823ada1ad98036f156c24
Mustafo Mustafokulov <maximusphoenix24@gmail.com>
2021-01-05 21:31:49 -0500

    Data upload

--- data.py
+++ data.py
@@ -1,7 +1,7 @@
-import collections
-import pathlib
-import re
-import string
+# import collections
+# import pathlib
+# import re
+# import string
 
 # import tensorflow as tf
 
@@ -10,16 +10,25 @@ import pandas as pd
 
 import json
 
-from tensorflow.keras import layers
-from tensorflow.keras import losses
-from tensorflow.keras import preprocessing
-from tensorflow.keras import utils
-from tensorflow.keras.layers.experimental.preprocessing import TextVectorization
+# from tensorflow.keras import layers
+# from tensorflow.keras import losses
+# from tensorflow.keras import preprocessing
+# from tensorflow.keras import utils
+# from tensorflow.keras.layers.experimental.preprocessing import TextVectorization
 
 # import tensorflow_datasets as tfds
 
 def import_data():
-    with open('./Revised.json') as f:
-        data = json.load(f)
+    # obj = json.loads('./Revised.json').decode('utf-8')
+    # bbox = obj['bounding_box']
+    # return np.array([bbox['x'], bbox['y'], bbox['height'], bbox['width']], dtype='f')
+    with open('./Revised.json') as json_file:
+        data = json.load(json_file)
+        # print(data[0]['text'])
+        # print(data)
+        for line in data:
+            # for p in line:
+            print(line)
+        # print(np.array(data))
 
 import_data()
