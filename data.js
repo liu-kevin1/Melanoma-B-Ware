417a0dc1eb5629069c128693d19d6273ee845e87
Hayun <YunJun505@gmail.com>
2021-01-12 11:22:45 -0500

    for loading model into website

--- data.js
+++ data.js
@@ -1,1 +1,9 @@
 // blank mustafo 
+import * as tf from '@tensorflow/tfjs';
+import {loadGraphModel} from '@tensorflow/tfjs-converter';
+
+const MODEL_URL = 'model-to-js/model.json';
+
+const model = await loadGraphModel(MODEL_URL);
+const cat = document.getElementById('cat');
+model.execute(tf.browser.fromPixels(cat));
