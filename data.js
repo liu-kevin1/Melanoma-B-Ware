53aa687648240d7bc165d587046451c310bc2f84
orgwirelist123 <orgwirelist123@gmail.com>
2021-01-13 15:07:14 -0500

    commented out requires and imports

--- data.js
+++ data.js
@@ -1,20 +1,25 @@
-// blank mustafo 
-var requirejs = require(['requirejs']);
-const tf = requirejs('@tensorflow/tfjs');
-const tf_converter = requirejs('@tensorflow/tfjs-converter');
-console.log(tf);
-const MODEL_URL = 'model-to-js/model.json';
+// // blank mustafo 
+// // var requirejs = require(['requirejs']);
 
-const model = tf_converter.loadGraphModel(MODEL_URL);
-// const cat = document.getElementById('cat');
-// model.execute(tf.browser.fromPixels(cat));
-// const example = 
-var submit = document.getElementById("submit");
-submit.onclick = function() {
-    var message = document.getElementById("message").value;
+// // require(['@tensorflow/tfjs'], function (foo) {
+// //     //foo is now loaded.
+// // });
+
+// // const tf = requirejs('@tensorflow/tfjs');
+// // const tf_converter = requirejs('@tensorflow/tfjs-converter');
+// console.log(tf);
+// const MODEL_URL = 'model-to-js/model.json';
+
+// const model = tf_converter.loadGraphModel(MODEL_URL);
+// // const cat = document.getElementById('cat');
+// // model.execute(tf.browser.fromPixels(cat));
+// // const example = 
+// var submit = document.getElementById("submit");
+// submit.onclick = function() {
+//     var message = document.getElementById("message").value;
     
-    var output = model.predict(message);
+//     var output = model.predict(message);
 
-    document.getElementById('output').innerText = output;    
-}
+//     document.getElementById('output').innerText = output;    
+// }
 
--- script.js
+++ script.js
@@ -1,1 +1,34 @@
-<button onclick ="myFunction()">Check.</button>
+// blank mustafo 
+// var requirejs = require(['requirejs']);
+
+// require(['@tensorflow/tfjs'], function (foo) {
+//     //foo is now loaded.
+// });
+
+// const tf = requirejs('@tensorflow/tfjs');
+// const tf_converter = requirejs('@tensorflow/tfjs-converter');
+//import * as tf from '@tensorflow/tfjs';
+
+const MODEL_URL = '/model-to-js/model.json';
+
+async function test() {
+    const model = tf.loadLayersModel(MODEL_URL);
+    return model;
+}
+
+// const cat = document.getElementById('cat');
+// model.execute(tf.browser.fromPixels(cat));
+// const example = 
+
+// var submit = document.getElementById("submit");
+// submit.onclick = function() {
+//     var message = document.getElementById("message").value;
+    
+//     var output = model.predict(message);
+
+//     document.getElementById('output').innerText = output;    
+// }
+
+
+var mod = test();
+console.log(mod);
